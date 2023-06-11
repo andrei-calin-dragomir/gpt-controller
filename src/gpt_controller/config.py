798b024dc6cd44983300e083512f3df8002fda5e
andrei-calin-dragomir <andrei.calin.dragomir@gmail.com>
2023-06-12 01:18:12 +0200

    Updated config template

---
+++ src/gpt_controller/config.py
@@ -0,0 +1,12 @@
+# Path to database (default: ./db.sqlite3)
+DB_URL = './db.sqlite3'
+
+# API configuration
+OPENAI_API_KEY = 'your_api_key'
+CHATGPT_MODEL = 'gpt-3.5-turbo'
+
+# Number of retries if a completion fails (eg. wrong/broken format) (default: 3)
+RETRIES_TO_ABORT='3'
+
+# Path to prompt files (default: ./prompts/)
+PROMPT_PATH = './prompts/'
